2c60b3218f6476f39ba63313f4c1018038c84406
Olayemi5 <Olayemistephen007@gmail.com>
2021-09-28 00:23:42 -0700

    Added missing file to project

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            c += 1
+        except ValueError:
+            continue
+        except TypeError:
+            continue
+    print()
+    return c
--- 0x05-python-exceptions/README.md
+++
@@ -1,3 +0,0 @@
-# 0x05-python-exceptions
----
-
